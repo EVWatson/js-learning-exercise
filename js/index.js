37e2fb289f33bb6ecd462d140c4d6a7f9846394b
EVWatson <emily.watson@myob.com>
2018-11-15 14:29:15 +1100

    ordering query selectors,event listeners and functions

--- js/index.js
+++ js/index.js
@@ -1,12 +1,32 @@
-const btn = document.querySelector('#btn1')
 
-btn.addEventListener('click', function(e) {
-  // console.log(e.target)
-  e.target.style.backgroundColor = 'purple'
-  alert("You clicked the button")
-})
+// 1. querySelectors
+// 2. event listeners
+// 3. functions
+
+// const btn = document.querySelector('#btn1')
+
+const input = document.querySelector('#titleInput')
+const title = document.querySelector('#title')
+
+// btn.addEventListener('click', function(e) {
+//   // console.log(e.target)
+//   e.target.style.backgroundColor = 'blue'
+//   e.target.classList.add('grow')
+//   alert("You ordered pizza!")
+// })
 
 // console.dir(btn)
 
 
 // commented sections: used to console log the event to view what happens in the brower when you click the button with the 'inspect' open. have to use console.dir for chrome, as well as the console.log in the addEventListener.
+
+input.addEventListener('input', changeText)
+
+function changeText(e) {
+  const text = e.target.value
+  title.innerText = text
+}
+
+function addColorToCharacter(char) {
+  const span = document.createElememnt('span')
+}
