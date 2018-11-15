41a5f72d99b871f4badba011443c4d93eaaac0f2
EVWatson <emily.watson@myob.com>
2018-11-15 12:53:13 +1100

    console.log for viewing what happens with event button; changed button background colour

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 const btn = document.querySelector('#btn1')
 
-btn.addEventListener('click', function() {
+btn.addEventListener('click', function(e) {
+  // console.log(e.target)
+  e.target.style.backgroundColor = 'purple'
   alert("You clicked the button")
 })
+
+// console.dir(btn)
+
+
+// commented sections: used to console log the event to view what happens in the brower when you click the button with the 'inspect' open. have to use console.dir for chrome, as well as the console.log in the addEventListener.
